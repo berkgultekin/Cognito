39a461e4e96a107a8cd68b22a7ba9ddd211d5e9c
berkgultekin <berk.gultekinn@gmail.com>
2022-05-01 15:18:55 +0300

    hata düzeltildi.

--- js/main.js
+++ js/main.js
@@ -119,10 +119,23 @@ function main() {
 
     });
 
-
-
 }());
 
+}
+
+
+//Get the button:
+mybutton = document.getElementById("toTheTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 main();
